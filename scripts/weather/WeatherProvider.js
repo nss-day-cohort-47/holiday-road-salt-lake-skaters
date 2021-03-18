e72cf7908d9a3e9fe9db4b6a88e21e29b8782652
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-18 15:07:46 -0500

    merging

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,56 +1,52 @@
 // imported API key 
 import { settings } from "../Settings.js"
-import {lat, long} from "../parks/ParkProvider.js"
+import { lat, long } from "../parks/ParkProvider.js"
 
 // array that holds copy of useWeatherCollection
 let weatherCollection = [];
 
 
 // function that returns copy of useWeatherCollection array
-export const useWeatherCollection =() => {
+export const useWeatherCollection = () => {
     return [...weatherCollection];
 }
 
 // created a function that fetchs forecast from API then return parsed array
+// used an if else statement to show nashville forecast if park is not selected
+// else show forecast for park area selected
 export const getWeather = () => {
-     if ( lat.length === 0 ){
-         return fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=36.1627&lon=-86.7816&exclude=current,minutely,hourly,alerts&units=imperial&appid=${settings.weatherKey}`)
-         .then(response => response.json())
-         .then(parsedResponse => {
-             weatherCollection = parsedResponse.daily.slice(0, 5)})}
+    if (lat.length === 0) {
+        return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=36.1627&lon=-86.7816&exclude=current,minutely,hourly,alerts&units=imperial&appid=${settings.weatherKey}`)
+            .then(response => response.json())
+            .then(parsedResponse => {
+                weatherCollection = parsedResponse.daily.slice(0, 5)
+            })
+    }
 
-     else {
-    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${settings.weatherKey}`)
-    .then(response => response.json())
-    .then(parsedResponse => {
-        weatherCollection = parsedResponse.daily.slice(0, 5)})
+    else {
+        return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${settings.weatherKey}`)
+            .then(response => response.json())
+            .then(parsedResponse => {
+                weatherCollection = parsedResponse.daily.slice(0, 5)
+            })
 
-       
+
     }
 }
 
 // create a function to convert timestamp to date 
 const convertTime = (date) => {
-    
 
-const newDate = new Date (date * 1000)
-let days = newDate.toLocaleDateString( "en-US",{ weekday: "long" });
-// Returns the date portion of a Date object as a string
 
-return days;
+    const newDate = new Date(date * 1000)
+    let days = newDate.toLocaleDateString("en-US", { weekday: "long" });
+    // Returns the date portion of a Date object as a string
+    // converts date to show weekday only
+
+    return days;
 }
 
 
-
-// try to extract day from date using the following example
-// public static String getDayStringNew(LocalDate date, Locale locale) {
-//     DayOfWeek day = date.getDayOfWeek();
-//     return day.getDisplayName(TextStyle.FULL, locale);
-// }
-
-
-
-  
 const ForecastCard = (weatherObj) => {
     console.log(weatherObj);
     return `
@@ -62,35 +58,21 @@ const ForecastCard = (weatherObj) => {
                 </ul>
     `
 }
-
- export const weatherList = () => {
-    // debugger
+// create and export function 
+export const weatherList = () => {
     let weatherHTML = "";
-    getWeather().then ( () => {
+    getWeather().then(() => {
         const weatherArray = useWeatherCollection();
         console.log(weatherArray);
-        for (const day of weatherArray ){
+        for (const day of weatherArray) {
             weatherHTML += ForecastCard(day)
-            
+
         }
         const weatherElement = document.querySelector(".forecast");
-    
-    getWeather().then(() => {
-        weatherElement.innerHTML = weatherHTML;
-    })
+
+        getWeather().then(() => {
+            weatherElement.innerHTML = weatherHTML;
         })
-    }
-    
-    
+    })
+}
 
-    // return weatherHTML;
-  
-
-
-//  export const showWeatherList = () => {
-    // const weatherElement = document.querySelector(".section-weather");
-    
-    // getWeather().then(() => {
-    //     weatherElement.innerHTML = weatherList();
-    // })
-// }
